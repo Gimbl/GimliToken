e4cf78f03b6dbf26a5dc164952842fbe537fe79f
Gimbl <contact@gimbl.gg>
2017-06-19 23:51:52 +0200

    clean useless files

--- compile.sh
+++ compile.sh
@@ -11,5 +11,12 @@ mkdir -p bin
 mkdir -p doc
 
 solc --abi --overwrite -o ../abi/ Gimli.sol
+find ../abi/ -type f ! -name 'Gimli.*' -delete
+
 solc --bin --overwrite -o ../bin/ Gimli.sol
+find ../bin/ -type f ! -name 'Gimli.*' -delete
+
 solc --userdoc --devdoc --overwrite -o ../doc/ Gimli.sol
+find ../doc/ -type f ! -name 'Gimli.*' -delete
+
+popd
